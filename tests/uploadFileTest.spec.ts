6106d6a40904869e7f79a443297f0e40a27a92d8
AlonLavy <alon.lavy2@gmail.com>
2024-06-27 16:42:44 +0300

    added missing files

---
+++ tests/uploadFileTest.spec.ts
@@ -0,0 +1,35 @@
+import { test, expect } from "@playwright/test";
+import path from "path";
+
+// test("has title", async ({ page }) => {
+//   await page.goto("https://playwright.dev/");
+
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright/);
+// });
+
+// test("get started link", async ({ page }) => {
+//   await page.goto("https://playwright.dev/");
+
+//   // Click the get started link.
+//   await page.getByRole("link", { name: "Get started" }).click();
+
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(
+//     page.getByRole("heading", { name: "Installation" })
+//   ).toBeVisible();
+// });
+
+test("upload file", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+
+  const fileChooserPromise = page.waitForEvent("filechooser");
+  await page.getByText("Drag 'n' drop some files here").click();
+  const fileChooser = await fileChooserPromise;
+  await fileChooser.setFiles(
+    path.join(
+      "C:\\Users\\Alon Lavy\\Desktop\\Personal - Alon\\dev\\maps\\src\\markers.json"
+    )
+  );
+  await expect(page.getByAltText("marker0")).toBeVisible();
+});
